8df060cb3ae36e0e5dd02385f41adbaa3113b9f4
Nathaniel Graham <nathaniel.graham@protonmail.com>
2021-08-25 14:35:25 -0600

    blacklist.sh

---
+++ blacklist.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+function usage() {
+cat << EOF
+Usage: $0 [OPTIONS]
+EOF
+}
+
+function header() {
+    local title=$1
+    local t_len=${#title}
+    local size=50
+    local pre_space_len=$((($size - $t_len)/2 - 1))
+    local pre_space=$(printf "%-${pre_space_len}s" " ")
+    local bar=$(printf "%-${size}s" "-")
+    echo "${bar// /-}"
+    echo "${pre_space// / }$title"
+    echo "${bar// /-}"
+    echo -e "$(date)\n"
+}
+function list_categories() {
+    # list blackarch groups
+    header "Blackarch Categories"
+    pacman -Qg | grep blackarch- | awk '{print $1}' | sort | uniq -c
+}
+
+function list_tools_in_category() {
+
+    header "$1 Tools"
+    local category=$1
+    local prefix="blackarch-"
+    category=${category#"$prefix"}
+    pacman -Qg | grep $prefix$category | awk '{print $2}' | sort
+}
+
+if [ $# -eq 0 ]; then
+    list_categories
+    exit 0
+fi
+
+while getopts h option; do
+    case "${option}"
+        in
+        h) usage; exit 0;;
+    esac
+done
+
+list_tools_in_category $1
